c5a758708bcd8adb8dc7d15ab7444e75496460bd
Fitzi <anton@m-fitzenreiter.de>
2025-07-05 15:18:24 +0200

    implemented particles for turrets and enemies

--- Assets/Scripts/SoundLibrary.cs
+++ Assets/Scripts/SoundLibrary.cs
@@ -3,7 +3,39 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "SoundLibrary", menuName = "Scriptable Objects/SoundLibrary")]
 public class SoundLibrary : ScriptableObject
 {
-    [SerializeField] public AudioClip musicMainScore;
-    [SerializeField] public AudioClip sfxTurretWristwatchArtilleryFire;
     
+
+    [Header("Music")]
+    [SerializeField] public AudioClip musicMainScore;
+
+    [Space(10)]
+
+    [Header("Soundeffects Turrets")]
+    [SerializeField] public AudioClip sfxTurretWristwatchArtilleryFire;
+    [SerializeField] public AudioClip sfxTurretWristwatchArtilleryHit;
+
+    [SerializeField] public AudioClip sfxTurretSundailLaserFire;
+    [SerializeField] public AudioClip sfxTurretSundailLaserHit;
+
+    [SerializeField] public AudioClip sfxTurretDigitalRocketlauncherFire;
+    [SerializeField] public AudioClip sfxTurretDigitalRocketlauncherHit;
+
+    [SerializeField] public AudioClip sfxTurretDroneFire;
+    [SerializeField] public AudioClip sfxTurretDroneHit;
+
+    [SerializeField] public AudioClip sfxTurretDroneStandFire;
+
+    [SerializeField] public AudioClip sfxTurretDynamiteFire;
+
+    [SerializeField] public AudioClip sfxTurretFreezeFire;
+
+    [Space(10)]
+
+    [Header("Soundeffects Busstop")]
+    [SerializeField] public AudioClip sfxBusstopHit;
+
+    [Space(10)]
+    [Header("Soundeffects UI")]
+    [SerializeField] public AudioClip sfxTurretPlaced;
+
 }
